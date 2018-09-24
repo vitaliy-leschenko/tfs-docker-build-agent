1e36b4e5d150ccfa7d00ccaf91a68e038e6bcae0
Виталий Лещенко <v.leschenko@vitaliy.org>
2018-09-24 22:44:10 +0300

    tfsagent-docker

--- run.cmd
+++ run.cmd
@@ -1,8 +1,9 @@
-docker run --name "tfs_agent" ^
-	-e VSTS_AGENTNAME="Windows Server (version 1809) (Docker)" ^
+docker run --name "tfs_agent_17758" ^
+	-e VSTS_AGENTNAME="Windows Server (version 1809 - 10.0.17758.1) (Docker)" ^
 	-e VSTS_ACCOUNT="https://tfs.vitaliy.org" ^
 	-e VSTS_TOKEN=%VSTS_TOKEN% ^
 	-e VSTS_POOL="Default" ^
-	--rm -it tfsagent-nodejs-dotnet:1809
+	-v \\.\pipe\docker_engine:\\.\pipe\docker_engine ^
+	--restart on-failure ^
+	-d tfsagent-docker:1809
 
-#	--restart on-failure ^
---
+++ tfsagent-docker/build.cmd
@@ -0,0 +1,1 @@
+docker build -t tfsagent-docker:1809 .
